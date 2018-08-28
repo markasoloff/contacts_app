48dd14b5933aaff2fc8ce9cc09b2ef343a5c83bd
“Marksoloff” <marksoloff@gmail.com>
2018-08-27 21:33:40 -0500

    rails contacts app

---
+++ app/controllers/api/contacts_controller.rb
@@ -0,0 +1,12 @@
+class Api::ContactsController < ApplicationController
+  def one_contact_action
+    @contact = Contact.first
+    render 'one_contact.json.jbuilder'
+  end
+
+  def many_contacts_action
+    @contacts = Contact.all
+    render 'many_contacts.json.jbuilder'
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  namespace :api do
+    get "/one_contact_url" => 'contacts#one_contact_action'
+    get "/many_contacts_url" => 'contacts#many_contacts_action'
+  end
+    
+
+  # STEP 1: A ROUTE triggers a controller action
+  # verb "/urls" => "namespace/controllers#action"
+end
