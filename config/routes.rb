6ab446182b691dbb4394ac45d19f623958e58f53
“Marksoloff” <marksoloff@gmail.com>
2018-08-30 21:33:13 -0500

    RESTful edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/one_contact_url" => 'contacts#one_contact_action'
-    get "/many_contacts_url" => 'contacts#many_contacts_action'
+    get "/contacts" => 'contacts#index'
+    post "/contacts" => 'contacts#create'
   end
     
 
