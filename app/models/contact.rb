0b903a274fdea7d63840d9e563612d6f54305151
“Marksoloff” <marksoloff@gmail.com>
2018-09-02 12:06:06 -0500

    partial views

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,15 @@
 class Contact < ApplicationRecord
 
+  def friendly_updated_at
+    created_at.strftime("%m/%d/%Y")
+  end
+
+  def full_name
+    "#{first_name}, #{last_name}"
+  end
+
+  def japan_phone_number
+    "+81 #{phone_number}"
+  end
+
 end
