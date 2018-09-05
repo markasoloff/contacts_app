c17daa1648262b1d5ed54f53aac28d69e8db8cf2
“Marksoloff” <marksoloff@gmail.com>
2018-09-04 21:33:29 -0500

    end of class push

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,8 @@
 class Contact < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: true
+  
 
   def friendly_updated_at
     created_at.strftime("%m/%d/%Y")
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Contact.create!([
+  {first_name: "Plutarch", last_name: "McGyver", email: "pdiddy@msichicago.org", phone_number: "123-456-789", middle_name: nil, bio: nil},
+  {first_name: "Duck", last_name: "Dodgers", email: "dd@google.com", phone_number: "333-333-3333", middle_name: nil, bio: nil},
+  {first_name: "pichu", last_name: "chan", email: "peechy@gmail.com", phone_number: "555-555-5556", middle_name: "ko", bio: nil},
+  {first_name: "Mark", last_name: "Soloff", email: "soloff@doozle.com", phone_number: "555-555-5555", middle_name: "Alexander", bio: "Mark is the greatest thinker of our times. He's the best. Incredible really. Wow."}
+])
